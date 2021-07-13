b976251e4f0d19ece6be0dbe9dc043ec0fcbd68a
jupiter06 <59487974+jyupiteru@users.noreply.github.com>
2021-07-14 03:37:01 +0900

    影を扱うクラスの追加 未完成

--- TeraTera3/ComSystem/Components/System/ComShadow/ComShadow.h
+++ TeraTera3/ComSystem/Components/System/ComShadow/ComShadow.h
@@ -5,6 +5,8 @@
  */
 
 #include "../../../../ComSystem/ComSystem.h"
+#include "../../Behavior/ComShader/ComShader.h"
+#include "../ComLight/ComLight.h"
 
 #pragma once
 
@@ -22,13 +24,20 @@ class ComShadow : public ComponentBase
 	 * @brief 影を描画したいオブジェクトの描画のみの関数を管理する変数
 	 * @n オブジェクト名、描画関数
 	 */
-	std::unordered_map<std::string, std::function<void(int)>> m_listObjectDrawFunction;
+	std::unordered_map<std::string, std::function<void(void)>> m_listObjectDrawFunction;
+
+	/**
+	 * @brief 各種コンポーネントへの簡易アクセス用変数
+	 */
+	ComShader *m_comShader = nullptr;
+	ComLight *m_comLight = nullptr;
 
 public:
 	ComShadow(){};
 	~ComShadow(){};
 	void Init() override;
 	void Uninit() override;
+	void Ready() override;
 	void Update() override;
 	void Draw() override;
 
@@ -39,8 +48,15 @@ public:
 	 */
 	void SetDrawShadowFuction(std::string_view _objname, std::function<void(void)> _function);
 
+	/**
+	 * @brief 関数を削除する処理
+	 * @param _objname 削除対象のオブジェクト
+	 */
 	void RemoveDrawFunction(std::string_view _objname);
 
 private:
+	/**
+	 * @brief シャドウマップを描画する処理
+	 */
 	void DrawShadowMap();
 };
