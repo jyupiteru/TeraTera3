c5a43b9fb558f13abd0853d721c0f585a7e11ee6
jupiter06 <59487974+jyupiteru@users.noreply.github.com>
2021-07-28 03:28:00 +0900

    影を適応するシェーダーをもとに戻す

--- TeraTera3/ComSystem/Components/Shape/ComBox/ComBox.cpp
+++ TeraTera3/ComSystem/Components/Shape/ComBox/ComBox.cpp
@@ -65,8 +65,8 @@ void ComBox::Init()
 	{
 		m_pComShader = m_gameObject->AddComponent<ComShader>();
 	}
-	m_pComShader->LoadVertexShader("VSShadow.fx", layout, numelements, true);
-	m_pComShader->LoadPixelShader("PSOnlyColorWithShadow.fx", true);
+	m_pComShader->LoadVertexShader("VS3dShape.fx", layout, numelements, true);
+	m_pComShader->LoadPixelShader("PSOnlyColor.fx", true);
 
 	m_comCount++;
 }
