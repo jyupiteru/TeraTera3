b1da4fd1df9ebc562e50a6624bd285861e7e8ffc
jupiter06 <59487974+jyupiteru@users.noreply.github.com>
2021-08-01 21:57:20 +0900

    少し処理を追加

--- TeraTera3/ComSystem/Components/Test/ComAssimpCollider/ComAssimpCollider.cpp
+++ TeraTera3/ComSystem/Components/Test/ComAssimpCollider/ComAssimpCollider.cpp
@@ -6,6 +6,7 @@
 #include "ComAssimpCollider.h"
 #include "../../../Core/GameObject.h"
 #include "../../Behavior/Com3DModelAssimp/Com3DModelAssimp.h"
+#include "../../Behavior/ComTransform/ComTransform.h"
 
 void ComAssimpCollider::Init()
 {
@@ -28,10 +29,25 @@ void ComAssimpCollider::Draw()
 
 void ComAssimpCollider::MakeAssimpCollider(void)
 {
+
+    auto rate = m_gameObject->m_transform->m_sizeRateSize.GetValue();
+
+    { //当たり判定の大きさをモデルと一緒にする
+        auto [firstSize_x, firstSize_y, firstSize_z] = m_gameObject->m_transform->m_offsetSize.GetValue();
+        firstSize_x *= rate;
+        firstSize_y *= rate;
+        firstSize_z *= rate;
+
+        m_finalSize.x = size_x * firstSize_x + offsize_x;
+        m_finalSize.y = size_y * firstSize_y + offsize_y;
+        m_finalSize.z = size_z * firstSize_z + offsize_z;
+    }
+
     auto mesh = m_gameObject->GetComponent<Com3DModelAssimp>()->GetModelData().modeldata.GetMeshes();
 
-    for (auto& itr : mesh)
+    for (auto &itr : mesh)
     {
+        itr.m_listBoneVertices;
     }
 
     //todo 当たり判定を構築する処理を追加!
